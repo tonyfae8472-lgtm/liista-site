a7f72a272cf19c71ef2bcce5c317d2249dca5977
tonyfae8472-lgtm <tonyfae8472@gmail.com>
2026-08-05 11:23:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 const meusProjetos = [
     { 
         nome: "Sobre sorvete", 
-        url: "sites/sobre-sorvete/index.html"  // Especifica o arquivo diretamente
+        url: "sites/sobre-sorvete/index.html" 
+    },
+    { 
+        nome: "História da BMW", 
+        url: "sites/historia-da-bmw/index.html" 
     }
 ];
 
 function renderizarPagina(projetosParaExibir) {
     const container = document.getElementById('conteudoPrincipal');
     container.innerHTML = '';
-
+    
     if (projetosParaExibir.length === 0) {
         container.innerHTML = `<div class="aviso-amarelo">Nada disponível :(</div>`;
         return;
     }
-
+    
     projetosParaExibir.forEach(projeto => {
         const div = document.createElement('div');
         div.classList.add('item-lista-site');
         div.innerText = projeto.nome;
         div.onclick = () => {
-            // Força o navegador a ir para a URL exata do arquivo HTML
             window.location.href = projeto.url;
         };
         container.appendChild(div);
